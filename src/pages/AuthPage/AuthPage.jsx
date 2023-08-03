db8d4ea62466e6f8fe98756ee5cf6ede808bd1d5
surysanchez <surysanchezsales@gmail.com>
2023-08-03 11:47:05 -0400

    update auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import { useState } from "react";
 
 export default function AuthPage({setUser}) {
-  
+  const [showForm, setShowForm] = useState(true)
+
   return (
     <> 
     <main>
+      <div>MERN - APP</div>
+      <button className="not-btn" onClick={() => setShowForm(!showForm)} >
+        {showForm ? "SIGN UP" : "SIGN IN"}
+        </button>
+      { showForm ? <SignUpForm  setUser={setUser} /> :  <LoginForm  setUser={setUser} /> }
     <div>AuthPage</div>
-    <SignUpForm setUser={setUser} />
-    < LoginForm setUser={setUser} />
+    {/* <SignUpForm setUser={setUser} />
+    < LoginForm setUser={setUser} /> */}
     </main>
     </>
   )
