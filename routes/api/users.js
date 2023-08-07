4d90561d118ef77efa4ea5e534002b7b2af98b33
surysanchez <surysanchezsales@gmail.com>
2023-08-07 09:01:05 -0400

    complete mern-infrastructure

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
+// require the authorization middleware function
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 // All path starts with 'api/users'
 // POST /api/users
